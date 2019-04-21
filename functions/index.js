93a1624dec05acdae346ee49ffd9bb09d5731389
cyuket <cyriluket12@gmail.com>
2019-04-21 12:20:53 +0100

    changes made

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,26 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.firebaseEmail = functions.https.onCall((request, response) => {
+  let email = request.email;
+  let name = request.name
+
+  var api_key = '40960b8e3e394b242a6487a4cc0e3159-2416cf28-2fbf2a7f';
+  var DOMAIN = 'sandbox34a1e715a75d4cc9943d6e90f05c53e6.mailgun.org';
+  var mailgun = require('mailgun-js')({
+    apiKey: api_key,
+    domain: DOMAIN
+  });
+
+  var data = {
+    from: 'Travel <godswill@gmail.com>',
+    to: `${name} <${email}>`,
+    subject: `Hello ${name}!`,
+    text: `Your message was succesfully sent !!!!`
+  };
+  mailgun.messages().send(data, function (error, body) {
+    if (error) {
+      console.log(error)
+    }
+    console.log(body);
+  });
+});
